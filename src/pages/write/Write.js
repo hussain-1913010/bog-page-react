a25e702d8bd8fd23b26d94c1665a549d4c0bc441
hussain-1913010 <hussain1913010@stud.kuet.ac.bd>
2021-09-14 20:17:42 +0600

    new post component created

---
+++ src/pages/write/Write.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./write.css";
+
+const Write = () => {
+  return (
+    <div className="write">
+        <img src="https://themetrust.com/wp-content/uploads/2018/06/blog-resource-cover-1000x500.jpg" alt="" className="writeImg"/>
+      <form action="" className="writeForm">
+        <div className="writeFormGroup">
+          <label htmlFor="fileInput">
+            <i className="writeIcon fas fa-plus"></i>
+          </label>
+          <input type="file" id="fileInput" style={{ display: "none" }} />
+          <input
+            type="text"
+            placeholder="Title"
+            className="writeInput"
+            autoFocus={true}
+          />
+        </div>
+        <div className="writeFormGroup">
+          <textarea
+            placeholder="Tell your Story ..."
+            type="text"
+            className="writeInput writeText"
+          ></textarea>
+        </div>
+        <button className="writeSubmit">Publish</button>
+      </form>
+    </div>
+  );
+};
+
+export default Write;

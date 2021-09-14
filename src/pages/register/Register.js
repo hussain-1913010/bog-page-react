03c9382eadc643ce9d56cd1d28af930f64c7d139
hussain-1913010 <hussain1913010@stud.kuet.ac.bd>
2021-09-14 21:00:02 +0600

    register page done

---
+++ src/pages/register/Register.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './register.css';
+const Register = () => {
+    return (
+        <div className="register">
+            <span className="registerTitle">Register</span>
+            <form className="registerForm">
+            <label htmlFor="">Username</label>
+                <input type="text" className="registerInput" placeholder="Enter Your Username"/>
+                <label htmlFor="">Email</label>
+                <input type="text" className="registerInput" placeholder="Enter Your Email"/>
+                <label htmlFor="">Password</label>
+                <input type="password" className="registerInput" placeholder="Enter Your Password"/>
+                <button className="registerButton">Register</button>
+                <button className="registerLoginButton">Login</button>
+            </form>
+        </div>
+    );
+};
+
+export default Register;
